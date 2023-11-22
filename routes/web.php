25a5d5d9988e57a3ab9515b310a2403c45e2638b
Bagas Mahda Dhani <muksinsaja12@gmail.com>
2023-11-22 19:17:04 +0700

    add: all Route

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,18 @@ use Illuminate\Support\Str; // import library Str
 |
 */
 
-$router->get('/mahasiswa', ['uses' => 'MahasiswaController@getAllMahasiswa']);
+$router->group(['prefix' => 'auth'], function () use ($router) {
+    $router->post('/register', ['uses'=> 'AuthController@register']);
+    $router->post('/login', ['uses'=> 'AuthController@login']); // route login
+});
+
+$router->group(['prefix' => 'mahasiswa'], function () use ($router) {
+    $router->get('/', [ 'uses' => 'MahasiswaController@getAllMahasiswa']);
+    $router->get('/profile', [ 'middleware' => 'jwt.auth' , 'uses' => 'MahasiswaController@getMahasiswaProfile']);
+    $router->get('/{nim}', [ 'uses' => 'MahasiswaController@getMahasiswaByNim']);
+    $router->post('/matakuliah/{mkId}', ['middleware' => 'jwt.auth', 'uses' => 'MahasiswaController@addMataKuliah']);
+    $router->put('/matakuliah/{mkId}', ['middleware' => 'jwt.auth', 'uses' => 'MahasiswaController@deleteMataKuliah']);
+});
+
 $router->get('/prodi', ['uses' => 'ProdiController@getAllProdi']);
 $router->get('/matakuliah', ['uses' => 'MataKuliahController@getAllMataKuliah']);
