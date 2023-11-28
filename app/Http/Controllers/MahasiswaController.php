8d62effeee49583ee428a95842515f73579b2f18
ardhiwahyuuu <ardhiwahyu@student.ub.ac.id>
2023-11-28 19:57:25 +0700

    Add : Mahasiswa/profile & Mahasiswa{nim}

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -28,4 +28,50 @@ class MahasiswaController extends Controller
             'data' => $mahasiswa 
         ]);
     }
+
+    // Ardhi Wahyu Hidayat - 215150707111038
+    public function getMahasiswaByNim(Request $request)
+    {
+        $mahasiswa = Mahasiswa::find($request->nim);
+
+        // Apabila mahasiswa tidak ditemukan
+        if (!$mahasiswa) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Mahasiswa not found',
+            ], 404);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Mahasiswa exist',
+            'mahasiswa' => [
+                'nim' => $mahasiswa->nim,
+                'nama' => $mahasiswa->nama,
+                'matakuliah' => $mahasiswa->matakuliahs
+            ]
+        ], 200);
+    }
+
+    // Ardhi Wahyu Hidayat - 215150707111038
+    public function getMahasiswaProfile(Request $request)
+    {
+        // Mendapatkan credentials token
+        $token = $request->header('token') ?? $request->query('token');
+        $credentials = JWT::decode($token, new Key(env('JWT_SECRET'), 'HS256'));
+        $nim = $credentials->sub;
+
+        $mahasiswa = Mahasiswa::with('prodi')->where("nim", $nim)->first();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Get Mahasiswa Profile',
+            'mahasiswa' => [
+                'nim' => $mahasiswa->nim,
+                'nama' => $mahasiswa->nama,
+                'angkatan' => $mahasiswa->angkatan,
+                'prodi' => $mahasiswa->prodi
+            ]
+        ], 200);
+    }
 }
