522e7bbff7957a63f092fae3e21df9f21c003cac
bariqalfariz <bariqalfariz09@gmail.com>
2023-11-28 20:01:37 +0700

    Add : Mahasiswa (addMataKuliah & deleteMataKuliah)

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -74,4 +74,68 @@ class MahasiswaController extends Controller
             ]
         ], 200);
     }
+
+    // Bariq Alfariz - 215150700111042
+    public function addMataKuliah(Request $request)
+    {
+        // Mendapatkan credentials token
+        $token = $request->header('token') ?? $request->query('token');
+        $credentials = JWT::decode($token, new Key(env('JWT_SECRET'), 'HS256'));
+        $nim = $credentials->sub;
+
+        $mahasiswa = Mahasiswa::find($nim);
+        
+        // Cek apakah mahasiswa sudah terdaftar pada mata kuliah
+        if ($mahasiswa->matakuliahs()->where('mkId', $request->mkId)->exists()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Mata Kuliah exist',
+            ], 400);
+        }
+        
+        $matakuliah = MataKuliah::find($request->mkId);
+
+        // Cek Apakah ditemukan Kode Mata Kuliah
+        if (!$matakuliah) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Mata Kuliah not found' 
+            ],404);
+        }
+
+        $mahasiswa->matakuliahs()->attach($matakuliah->id);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Mata Kuliah added',
+        ], 200);
+    }
+
+    // Bariq Alfariz - 215150700111042
+    public function deleteMataKuliah(Request $request)
+    {
+        // Mendapatkan credentials token
+        $token = $request->header('token') ?? $request->query('token');
+        $credentials = JWT::decode($token, new Key(env('JWT_SECRET'), 'HS256'));
+        $nim = $credentials->sub;
+        
+        $mahasiswa = Mahasiswa::find($nim);
+
+        $matakuliah = MataKuliah::find($request->mkId);
+
+        // cek apakah ada id Mata Kuliah
+        if (!$matakuliah) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Mata Kuliah not found' 
+            ],404);
+        }
+
+        $mahasiswa->matakuliahs()->detach($matakuliah->id);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Mata Kuliah Removed',
+        ], 200);
+    }
 }
